7caf18c4210528250899358f9a61680e0e006782
Robin Kataria <robin8kataria@gmail.com>
2020-05-16 19:54:02 +0530

    landing page title small

--- src/components/LandingPageComponents/middle/Panel1.jsx
+++ src/components/LandingPageComponents/middle/Panel1.jsx
@@ -8,8 +8,11 @@ function Panel1(){
         <div className="p-1 p-md-4 p-lg-4">
             <div className="row m-0 d-flex justify-content-center align-items-center" style={{height:'80vh'}}>
                 <div className="col-12 col-md-6 col-lg-6 d-flex flex-column align-items-center justify-content-center ">
-                    <h1 className="display-4"><FontAwesomeIcon icon={faFile}/><b>N</b>otesKeeper<span style={{fontSize:'1.5rem'}}>.md</span></h1>
-                    <p className="lead">Write in Markdown and Read in MarkUp </p>
+                    <h3><FontAwesomeIcon icon={faFile}/><b>N</b>otesKeeper<span className='fm'>.md</span></h3>
+                    <div className="lead text-break">
+                        <span>Write in Markdown , </span>
+                        <span>Read in MarkUp</span> 
+                    </div>
                     <div className="m-0" >
                         <hr />
                         <p style={{textAlign:'center'}}>Create NoteBooks and Store your Notes inside them.</p>
