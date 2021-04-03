c6cd2ffabc0c70d790ba2ba40372264db45a8ee8
Sharvan Sharma <sharvansharma518@gmail.com>
2021-04-04 02:29:49 +0530

    [SS-NOT-2]: Fixed Team section on landing page

--- src/components/LandingPageComponents/Team.jsx
+++ src/components/LandingPageComponents/Team.jsx
@@ -1,35 +1,54 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-const developers = [
-    {
-        link:'https://github.com/robinkataria',
-        src:'https://media-exp1.licdn.com/dms/image/C5603AQFhcK-HZTZ0xA/profile-displayphoto-shrink_400_400/0?e=1605139200&v=beta&t=mENzYdZGGhXMKf4KyUGR7ovgt4LO1l1Uzy_8ph7EQh8'
-    },
-    {
-        link:'https://github.com/sharvan-sharma',
-        src:'https://avatars0.githubusercontent.com/u/22548047?s=460&u=e7b878f436e600a29b3feaac7e70fff5a03c9a1f&v=4'
-    }
-    
-]
+const OWNER = "robinkataria";
+const REPOSITORY_NAME = "NotesKeeper.md-FRONTEND";
+const GITHUB_REPOSITORY_CONTRIBUTORS_URL = `https://api.github.com/repos/${OWNER}/${REPOSITORY_NAME}/contributors`;
+const DEPENDABOT_LOGIN = 'dependabot[bot]'
 
 export default function Team(){
+    const [contributors, setContributors] = useState([]);
+
+    useEffect(() => {
+        axios.get(GITHUB_REPOSITORY_CONTRIBUTORS_URL, {
+            withCredentials:false
+        })
+        .then((response) => { 
+            let contributors = [];
+            response.data.forEach(contributor => {
+                if (contributor["login"] !== DEPENDABOT_LOGIN) {
+                    contributors.push({
+                        login: contributor["login"],
+                        src: contributor["avatar_url"],
+                        link: contributor["html_url"]
+                    });
+                }
+            });
+            setContributors(contributors);
+        })
+        .catch(() => {})
+    }, [])
+
+    if (contributors.length === 0) {
+        return <></>
+    }
+
     return (
         <div className='col-12 d-flex justify-content-center align-items-center' style={{minHeight:'60vh'}}>
             <div className='col-12 col-lg-8 col-md-10 d-flex flex-column align-items-center'>
                 <h3 className='ff-mst fl bold'>Team</h3>
-                <p className='text-muted'>Meet our team of Developers</p>
+                <p className='text-muted'>Meet our Contributors</p>
                 <div className='d-flex flex-wrap justify-content-center my-4'>
                     {
-                        developers.map((developer,index) =>{ 
+                        contributors.map((contributor,index) =>{ 
                         return (
-                            <a href={developer.link} key = {index}>
-                                <img src={developer.src} style={{width:'100px'}} className='shadow img-fluid rounded-circle mx-4' />
+                            <a href={contributor.link} key = {index}>
+                                <img src={contributor.src} alt={contributor.login} style={{width:'100px'}} className='shadow img-fluid rounded-circle mx-4' />
                             </a>)
                         })
                     }
                 </div>
             </div>
         </div>
-
     )
 }
